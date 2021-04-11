4342080b079f2766a7fc9d582102af95b449720a
Jason Humphrey <sjasonhumphrey@yahoo.com>
2021-04-11 14:04:02 -0500

    Homework assignment 3 function in python.

---
+++ Python/Homework003/Homework003.py
@@ -0,0 +1,64 @@
+# Homework
+# Functions
+# 1. Find the smallest number
+# Write a function smallest that accepts a List of numbers as an argument.
+
+# It should return the smallest number in the List.
+
+list = [25, 43, 72, 19, 12, 93]
+
+def smallest(numbers):
+    smallest = numbers[0] if numbers else None
+    for i in numbers:
+        if i < smallest:
+                smallest = i
+    print("The smallest number is: ", smallest)
+
+smallest(list)
+
+# 2. Find the largest number
+# Write a function largest that accepts a List of numbers as an argument.
+
+# It should return the largest number in the List.
+
+list = [25, 43, 72, 19, 12, 93]
+
+def largest(numbers):
+    largest = numbers[0] if numbers else None
+    for i in numbers:
+        if i > largest:
+                largest = i
+    print("The largest number is: ", largest)
+
+largest(list)
+
+# 3. Find the shortest String
+# Write a function shortest that accepts a List of Strings as an argument.
+
+# It should return the shortest String in the List.
+
+words = ['Tennessee', 'Texas', 'Kentucky', 'Alabama', 'Ohio']
+
+def shortest(strings):
+    for i in strings:
+        if len(i) < len(strings):
+            shortest = i
+            print('This is the shortest string:',shortest)
+
+shortest(words)
+
+# 4. Find the longest String
+# Write a function longest that accepts a List of Strings as an argument.
+
+# It should return the longest String in the list.
+
+words =['Tennessee', 'Texas', 'Kentucky', 'Alabama', 'Ohio']
+
+def longest(strings):
+    longest = ''
+    for i in strings:
+        if len(i) > len(longest):
+            longest = i
+            print('This is the longest string:', longest)
+
+longest(words)
